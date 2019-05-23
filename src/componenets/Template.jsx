af71c931648ee88316d5c961445e9c988b91c642
arefeh-htmi <hatamiarefeh.ah@gmail.com>
2019-05-23 20:32:00 +0430

    base

--- src/componenets/Template.jsx
+++ src/componenets/Template.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import './style.css';
 class Template extends Component {
   constructor(props) {
     super(props);
@@ -11,10 +12,11 @@ class Template extends Component {
         { name: "3", mail: "3.", id: 3 },
         { name: "4", mail: "4.", id: 4 },
         { name: "5", mail: "5", id: 5 }
-      ]
+      ],
+      toEditIndex :0
     };
     this.handleChangeInput = this.handleChangeInput.bind(this);
-    this.handleAdd = this.handleAdd.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
     this.editModeStatus = this.editModeStatus.bind(this);
     // this.handleInputEdit = this.handleInputEdit.bind(this);
@@ -31,7 +33,6 @@ class Template extends Component {
   return buttonInnerText;
   }
   handleChangeInput(e) {
-    if (this.state.editMode === false) {
       const { name, value } = e.target;
       let newItems = {
         ...this.state.items,
@@ -42,21 +43,20 @@ class Template extends Component {
         ...this.state.items,
         items: newItems
       });
-    }
-    if (this.state.editMode === true) {
-      this.handleEdit(x);
-    }
   }
   /* will handle Create part of CRUD => Add new data to array */
-  handleAdd(e) {
-    if (this.state.editMode === true) {
+  handleSubmit(e) {
+    if (this.state.editMode === false) {
       e.preventDefault();
       this.state.myData.push(this.state.items);
       this.setState({ items: { name: "", mail: "", id: "" } });
     }
     if (this.state.editMode === true) {
-      this.setState({ editMode: false  });
       console.log('updated')
+      // this.setState({ myData:  });
+    // this.setState({ toEditIndex: 0 });
+
+      this.setState({ editMode: false  });
     }
   }
   /* will find the clicked list item of html tags inside the render method */
@@ -71,24 +71,17 @@ class Template extends Component {
     this.state.myData.splice(this.findClickedIndex(id), 1);
   }
   /* will recieve the id of selected Element and edit it */
-
-  handleInputEdit(e) {
-    console.log(e.target.value);
-
-
-  }
-  handleEdit(id,e) {
+  handleEdit(id) {
     this.setState({ editMode: true  });
     const obj=this.state.myData[this.findClickedIndex(id)];
-    console.log(obj)
-
-
-    // console.log();
+    this.setState({ items: { name: obj.name, mail: obj.mail, id: obj.id } });
+    const index =this.findClickedIndex(id);
+    this.setState({ toEditIndex: index });
   }
   render() {
     return (
       <>
-        <form action="" onSubmit={this.handleAdd}>
+        <form action="" onSubmit={this.handleSubmit}>
           <label htmlFor="name">Name:</label>
           <input
             type="text"
