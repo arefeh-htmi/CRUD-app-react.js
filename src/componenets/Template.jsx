3bafc85612bbbc4c9b6f12e8ffafe392a74cedfe
arefeh-htmi <hatamiarefeh.ah@gmail.com>
2019-05-25 09:56:00 +0430

    fullCRUD

--- src/componenets/Template.jsx
+++ src/componenets/Template.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './style.css';
+import "./style.css";
 class Template extends Component {
   constructor(props) {
     super(props);
@@ -7,32 +7,38 @@ class Template extends Component {
       editMode: false,
       items: { name: "", mail: "", id: "" },
       myData: [
-        { name: "1", mail: "1", id: 1 },
-        { name: "2", mail: "2", id: 2 },
-        { name: "3", mail: "3.", id: 3 },
-        { name: "4", mail: "4.", id: 4 },
-        { name: "5", mail: "5", id: 5 }
+        { name: "n1", mail: "m1", id: 1 },
+        { name: "n2", mail: "m2", id: 2 },
+        { name: "n3", mail: "m3.", id: 3 },
+        { name: "n4", mail: "m4.", id: 4 },
+        { name: "n5", mail: "m5", id: 5 }
       ],
-      toEditIndex :0
+      toEditId: 0,
     };
     this.handleChangeInput = this.handleChangeInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleDelete = this.handleDelete.bind(this);
     this.editModeStatus = this.editModeStatus.bind(this);
-    // this.handleInputEdit = this.handleInputEdit.bind(this);
   }
   /* will handle every change on input */
-  editModeStatus(){
+  editModeStatus() {
     let buttonInnerText;
-    if(this.state.editMode=== true){
-  buttonInnerText= "update "    
+    if (this.state.editMode === true) {
+      buttonInnerText = "update ";
     }
-    if(this.state.editMode=== false){
-      buttonInnerText= "add"
+    if (this.state.editMode === false) {
+      buttonInnerText = "add";
+    }
+    return buttonInnerText;
   }
-  return buttonInnerText;
+  /* will find the clicked list item of html tags inside the render method */
+  findClickedIndex(id) {
+    const index = this.state.myData.findIndex(item => item.id === id);
+    return index;
   }
+  /* will handle changes on inputes on the page */
   handleChangeInput(e) {
+    if (this.state.editMode === false) {
       const { name, value } = e.target;
       let newItems = {
         ...this.state.items,
@@ -43,27 +49,35 @@ class Template extends Component {
         ...this.state.items,
         items: newItems
       });
+    }
+    if (this.state.editMode === true) {
+      const { name, value } = e.target;
+      let newItems = {
+        ...this.state.items,
+        [name]: value,
+        id: this.state.toEditId
+      };
+      this.setState({
+        ...this.state.items,
+        items: newItems
+      });
+    }
   }
   /* will handle Create part of CRUD => Add new data to array */
   handleSubmit(e) {
     if (this.state.editMode === false) {
       e.preventDefault();
       this.state.myData.push(this.state.items);
+      console.log(this.state.myData);
       this.setState({ items: { name: "", mail: "", id: "" } });
     }
     if (this.state.editMode === true) {
-      console.log('updated')
-      // this.setState({ myData:  });
-    // this.setState({ toEditIndex: 0 });
-
-      this.setState({ editMode: false  });
+      e.preventDefault();
+      const id = this.state.toEditId;
+      this.state.myData.splice(this.findClickedIndex(id), 1, this.state.items);
+      this.setState({ items: { name: "", mail: "", id: "" } });
+      this.setState({ editMode: false });
     }
-  }
-  /* will find the clicked list item of html tags inside the render method */
-
-  findClickedIndex(id) {
-    const index = this.state.myData.findIndex(item => item.id === id);
-    return index;
   }
   /* will recieve the id of selected Element and delete it */
   handleDelete(id) {
@@ -72,50 +86,65 @@ class Template extends Component {
   }
   /* will recieve the id of selected Element and edit it */
   handleEdit(id) {
-    this.setState({ editMode: true  });
-    const obj=this.state.myData[this.findClickedIndex(id)];
-    this.setState({ items: { name: obj.name, mail: obj.mail, id: obj.id } });
-    const index =this.findClickedIndex(id);
-    this.setState({ toEditIndex:index });
+    this.setState({ editMode: true });
+    const obj = this.state.myData[this.findClickedIndex(id)];
+    this.setState({ items: obj });
+    this.setState({ toEditId: id });
   }
   render() {
     return (
       <>
         <form action="" onSubmit={this.handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            className="name"
-            placeholder="Name"
-            name="name"
-            value={this.state.items.name}
-            onChange={this.handleChangeInput}
-          />
-          <label htmlFor="mail">Mail:</label>
-          <input
-            type="text"
-            id="mail"
-            className="mail"
-            placeholder="Mail"
-            name="mail"
-            value={this.state.items.mail}
-            onChange={this.handleChangeInput}
-          />
-         
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              className="name"
+              placeholder="Name"
+              name="name"
+              value={this.state.items.name}
+              onChange={this.handleChangeInput}
+            />
+          </div>
+          <div>
+            <label htmlFor="mail">Mail:</label>
+            <input
+              type="text"
+              id="mail"
+              className="mail"
+              placeholder="Mail"
+              name="mail"
+              value={this.state.items.mail}
+              onChange={this.handleChangeInput}
+            />
+          </div>
+
           <button id={this.editModeStatus()}>{this.editModeStatus()}</button>
         </form>
 
         <ol>
+          <li style={{padding: `20px 260px 20px 0`}}>
+            <span>Name</span>
+            <span>Mail</span>
+          </li>
           {this.state.myData.map((el, id) => (
-            <li key={id}>
-              {el.name} /{el.mail}
+            <li key={id} style={{padding: `20px 260px 20px 0`}}>
+              <div>
+              {el.name}
+              </div>
+              <div>
+              {el.mail}
+              </div>
+              <div className="btnContainer" btncontainerwidth={
+                console.log()}>
               <button id="edit" onClick={() => this.handleEdit(el.id)}>
                 Edit
               </button>
               <button id="delete" onClick={() => this.handleDelete(el.id)}>
                 Delete
               </button>
+              </div>
             </li>
           ))}
         </ol>
